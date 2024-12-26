675ea1ff8621d8ebac22305e8dc2ffb1334264b6
Mohit Sethi <m.sethi006@gmail.com>
2024-12-26 14:49:53 +0530

    Minor Change

--- app1.py
+++ app1.py
@@ -6,7 +6,6 @@ import json
 with open("config.json", "r") as file:
     config = json.load(file)
 
-# Example usage
 bucket_name = config["bucket_name"]
 aws_access_key_id = config["aws_access_key_id"]
 aws_secret_access_key = config["aws_secret_access_key"]
@@ -20,7 +19,6 @@ for file in files:
 
 file = '2020_03_01.csv'
 df = download_and_read_csv_from_s3(bucket_name, file, aws_access_key_id, aws_secret_access_key, aws_region)
-# Check if columns match
 df_columns = df.columns.tolist()
 missing_in_df = set(sql_columns) - set(df_columns)
 extra_in_df = set(df_columns) - set(sql_columns)
@@ -29,24 +27,19 @@ if not missing_in_df and not extra_in_df:
     print(f"{file} All columns match the SQL structure!")
     df['started_at'] = pd.to_datetime(df['started_at'])
     df['ended_at'] = pd.to_datetime(df['ended_at'])
-    # Replace with your PostgreSQL credentials and EC2 details
     username = config["username"]
     password = config["password"]
     host = config["host"]
-    port = config["port"]  # Default PostgreSQL port
+    port = config["port"]  
     database = config["database"]
 
-    # Create PostgreSQL connection string
     connection_string = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 
-    # Create SQLAlchemy engine
     engine = create_engine(connection_string)
     #df.to_sql('citibike_trips', con=engine, if_exists='append', index=False)
-    # Select specific index values
-    selected_indices = [2, 5, 8]  # Replace with the indices you want
+    selected_indices = [2, 5, 8]  
     filtered_df = df.loc[selected_indices]
 
-    # Insert the filtered DataFrame into the SQL table
     table = config["table"]
     filtered_df.to_sql(table, con=engine, if_exists='append', index=False)
 
